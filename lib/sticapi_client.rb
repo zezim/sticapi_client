96ec137931c85f0241d1f0089c3127e0d255e293
Ricardo Viana <zezim.ricardo@gmail.com>
2017-09-25 12:00:09 -0300

    fix: valores de teste

--- lib/sticapi_client.rb
+++ lib/sticapi_client.rb
@@ -20,8 +20,8 @@ module SticapiClient
     attr_accessor :expiry
 
     def initialize
-      # configs = YAML.load_file("#{Rails.root}/config/sticapi.yml")[Rails.env]
-      configs = YAML.load_file("/home/ricardo/dev/sticapi_client/lib/generators/sticapi_client/templates/sticapi.yml")[Rails.env]
+      configs = YAML.load_file("#{Rails.root}/config/sticapi.yml")[Rails.env]
+      # configs = YAML.load_file("/home/ricardo/dev/sticapi_client/lib/generators/sticapi_client/templates/sticapi.yml")[Rails.env]
       @host = configs['host']
       @port = configs['port']
       @user = configs['user']
